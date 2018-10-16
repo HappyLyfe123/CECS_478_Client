82b8a534ebae0c7a9779c6240bf10f74561d02ea
jakeparcell14 <jakeparcell14@gmail.com>
2018-10-16 00:01:11 -0700

    Add files via upload

--- encrypter.py
+++ encrypter.py
@@ -2,28 +2,39 @@
 # The output of the module will be RSA ciphertext, AES ciphertext and HMAC tag based on the input
 
 from Crypto.PublicKey import RSA
-import random
+from Crypto.Cipher import AES
+import os
+import hmac, hashlib
 
 
 def encrypt_message(message, public_key_path):
 
 #RSA Encryption
-    print("running")
-
-    # generate RSA object
-    public_key = RSA.generate(2048)
 
     #extract public key from file path
-    public_key = importKey(open(public_key_path, 'r').read())
+    public_key = RSA.importKey(open(public_key_path, 'r').read())
 
     # set RSA encryption protocol
-    cipher = PKCS2_OAEP.new(public_key)
-
-    #encrypt message using RSA key
-    ciphertext_rsa = cipher.encrypt(message, 16)
+    rsa_cipher = PKCS2_OAEP.new(public_key)
 
 
 #AES Encryption
     # iv generation referenced from https://www.novixys.com/blog/using-aes-encryption-decryption-python-pycrypto/#4_Encrypting_with_AES
     iv = ''.join([chr(random.randint(0, 0xFF)) for i in range(16)])
+
+    aes_key = os.urandom(32)
+
+    aes_cipher = AES.new(aes_key, AES.MODE_CBC, iv)
+
+    # encrypt message
+    ciphertext_aes = iv + aes_cipher.encrypt(message)
+
+    # create HMAC tag
+    hmac_key = os.urandom(32)
+
+    hmac_object = hmac.new(hmac_key, ciphertext_aes, digestmod=hashlib.sha256)
+
+    # concatenate aes and hmac keys
+    keys = aes_key + hmac_key
+
 
