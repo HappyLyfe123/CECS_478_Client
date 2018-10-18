94c9d842197dd9d412eec1c99719392bcfbe9fc6
happylyfe123 <visalhok123@gmail.com>
2018-10-18 15:21:21 -0700

    Decrpty Message

--- Decrypter.py
+++ Decrypter.py
@@ -5,20 +5,24 @@ import json
 import base64
 import zlib
 
+private_key = RSA.importKey(open('private.pem','r').read())
+print(private_key)
+
 
 def decrypt_message(encrypt_message, key):
     #Set the aes object for decryption
-    iv = encrypt_message[0:16]
+    encrypt_message = base64.b64decode(encrypt_message)
+    iv = encrypt_message[:16]
     aes = AES.new(key, AES.MODE_CBC, iv)
-    decrypt_message = aes.decrypt(encrypt_message)
+    decrypt_message = aes.decrypt(encrypt_message[16:])
 
     return decrypt_message
 
 def get_aes_key(rsa_cipher_text):
-    private_key = RSA.importKey("private.pem", passphrase="AndrewV34")
+    private_key = RSA.importKey(open('private.pem','r').read())
     return private_key.decrypt(rsa_cipher_text)
 
-def get_keys(path):
+def get_data_json(path):
     loaded_json = json.load(path)
     rsa_cipher_text = loaded_json[0]
     encrypt_message = loaded_json[1]
--- Tester.py
+++ Tester.py
@@ -2,7 +2,7 @@ import encrypter
 import Decrypter
 from Crypto.PublicKey import RSA
 
-private_key_string = open("private.pem","r")
+private_key_string = open("private.pem","r").open()
 private_key = RSA.importKey(private_key_string, passphrase="AnderV34")
 print(private_key_string)
 #encrypt_message = encrypter.encrypt_message()
